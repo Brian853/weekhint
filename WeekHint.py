c5abfdda25453edfc65a7102ebebbc28aaee389e
Rainice Wu <rainice@live.com>
2018-08-09 00:40:33 +0800

    Update WeekHint.py
     - 修改标签行为，改善透明度，去掉白边，通过标签移动窗口
     - 移除画布
     - 修改函数名，共性的操作函数化
     - 精简代码

--- WeekHint.py
+++ WeekHint.py
@@ -1,43 +1,37 @@
-# -*- coding: UTF-8 -*-
 import tkinter,threading
 from datetime import datetime
 import tkinter.font as tkFont
-ww='60'
-hh='30'
+ww='50'
+hh='20'
 root = tkinter.Tk()
 root.overrideredirect(True)
-root.attributes('-alpha', 0.5)
-root.geometry(ww+'x'+hh+'+400+400')
+root.attributes('-alpha', 0.6)
 root.attributes('-topmost',1)
-canvas = tkinter.Canvas(root)
-canvas.configure(width = ww,height = hh,bg = 'black')
+root.geometry(ww+'x'+hh+'+400+400')
 ftlbl = tkFont.Font(family='fixedsys', size= 18)
-tmlbl = tkinter.Label(root,fg = '#FFFAFA',bg = 'black',text = 'Hello',anchor='center',font=ftlbl)
+tmlbl = tkinter.Label(root,width=200,fg = '#FFFAFA',bg = 'black',text = 'Hello',anchor='center',font=ftlbl)
 tmlbl.pack()
-canvas.configure(highlightthickness = 0)
-canvas.pack()
 x, y = 0, 0
-def move(event):
+def fun_move(event):
     global x,y
     new_x = (event.x-x)+root.winfo_x()
     new_y = (event.y-y)+root.winfo_y()
-    s = ww+'x'+hh +'+'+ str(new_x)+'+' + str(new_y)
-    root.geometry(s)
-def button_1(event):
+    root.geometry(ww+'x'+hh +'+'+ str(new_x)+'+' + str(new_y))
+def fun_button_1(event):
     global x,y
     x,y = event.x,event.y
 def fun_timer():
     global timer
-    tmlbl.configure(text = fun_getweek())
+    fun_applyweek()      
     timer = threading.Timer(5555, fun_timer)
     timer.start()
 def fun_getweek():
-    weeks = datetime.date(datetime.now()).isocalendar()[1]
-    return weeks
-canvas.bind('<B1-Motion>',move)
-canvas.bind('<Button-1>',button_1)
-year = str(datetime.now().year)[-2:]
-tmlbl.configure(text = year +'W'+str(fun_getweek()))
+    return datetime.date(datetime.now()).isocalendar()[1]
+def fun_applyweek():
+    tmlbl.configure(text = str(datetime.now().year)[-2:] +'W'+str(fun_getweek()))
+tmlbl.bind('<B1-Motion>',fun_move)
+tmlbl.bind('<Button-1>',fun_button_1)
+fun_applyweek() 
 root.mainloop()
 timer = threading.Timer(1, fun_timer)
 timer.start()
